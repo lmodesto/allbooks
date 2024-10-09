5699242fedbbfbf189e31f4017b3a3856694188e
Levi Modesto <levimodesto@gmail.com>
2024-10-08 23:04:21 -0300

    altera o servidor

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Boas vindas a API do allbooks")
+  console.log("API disponível traves da url em http://localhost:8000")
 })
